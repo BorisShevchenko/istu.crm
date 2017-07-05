8351679da77c55149cd85774fe4c0801c3bce392
Evgeny Cherkashin <eugeneai@irnok.net>
2017-07-05 12:41:19 +0800

    Adapted Scite for executing by F5 key.

--- src/istu/crm/__init__.py
+++ src/istu/crm/__init__.py
@@ -1,7 +1,59 @@
 # Example package with a console entry point
 from __future__ import print_function
 
+from pyramid.config import Configurator
+from pyramid.response import Response
+from wsgiref.simple_server import make_server
 
-def main():
-    app = 1
-    return app
+
+def hello_world(request):
+    return Response('<body><h1>Hello World!</h1></body>')
+
+
+def hi_body(request):
+    print(request.GET)
+    name = request.GET.get("name", "Unknown person")
+    return Response('Hi, {}!'.format(name))
+
+
+def ask(request):
+    q = request.POST.get("q", '').strip()
+    if q == '':
+        return Response("""
+        <form action="ask" method="POST">
+           <input name="q" placeholder="Делай запрос, чувак!"> </br>
+           <input name="submit" type="submit" value="Ok">
+        </form>
+        """)
+    else:
+        return Response("""
+        <h1>Hi, {q}!</h1>
+        <p>Привет, {q}!<p>
+        """.format(q=q))
+
+
+def main(global_config, **settings):
+    config = Configurator(settings=settings)
+
+    config.add_route('hello', '/')
+    config.add_view(hello_world, route_name='hello')
+
+    config.add_route('hi', '/hi')
+    config.add_view(hi_body, route_name='hi')
+
+    config.add_route('ask', '/ask')
+    config.add_view(ask, route_name='ask')
+
+    return config.make_wsgi_app()
+
+NET = '0.0.0.0'
+PORT = 6543
+
+def run():
+    app = main(None)
+    print("http://127.0.0.1:{}".format(PORT))
+    server = make_server(NET, PORT, app)
+    server.serve_forever()
+
+if __name__=="__main__":
+    run()
---
+++ src/istu/crm/__main__.py
@@ -0,0 +1,4 @@
+from istu.crm import run
+
+if __name__ == '__main__':
+    run()
